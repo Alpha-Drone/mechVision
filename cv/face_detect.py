55de9688cff12fb6674584f773b134d3250e5a99
Devin Mozee <alonzo.mozee@gmail.com>
2017-04-27 12:13:34 -0400

    Add comments

--- cv/face_detect.py
+++ cv/face_detect.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cv2
 
+#load in cascades
 face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
 
+#Capture video source
 vid = cv2.VideoCapture(0)
 
 while True:
 
     ret, frame = vid.read()
 
-    if ret:
+    if ret: #let our camera warm up
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        #for every face draw a square
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x,y),(x+w,y+h),(255,0,0), 2)
