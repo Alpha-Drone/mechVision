9d7a8bfa542ce11d05740dcb02fe346bbd840526
Devin Mozee <alonzo.mozee@gmail.com>
2017-05-01 12:29:40 -0400

    Add eye dectection/cascade/arguments

--- cv/face_detect.py
+++ cv/face_detect.py
@@ -1,5 +1,3 @@
-import numpy as np
-import cv2
 '''
  Title: AP Face Detection
  Files: cv/cascades
@@ -15,29 +13,46 @@ for source pack.
 
 Cascades are available in our repo: https://github.com/Alpha-Drone/mechVision
 '''
+
+import numpy as np
+import cv2
+import argparse
+
+#create argument for vision command
+ap = ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", required=False, help="video source")
+args = vars(ap.parse_args())
+
 #load in cascades
 face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("cascades/haarcascade_eye.xml")
 
-#Capture video source
-vid = cv2.VideoCapture(0) #change input base on computer
 
-while True:
 
-    ret, frame = vid.read()
+if args['video']:
 
-    if ret: #let our camera warm up
+    #Capture video source
+    vid = cv2.VideoCapture(int(args["video"])) #change input base on computer
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while True:
 
-        #for every face draw a square
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame, (x,y),(x+w,y+h),(255,0,0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
+        ret, frame = vid.read()
 
-        cv2.imshow('video',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-vid.release()
-cv2.destroyAllWindows()
+        if ret: #let our camera warm up
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            #for every face draw a square
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame, (x,y),(x+w,y+h),(255,0,0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
+            cv2.imshow('video',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    vid.release()
+    cv2.destroyAllWindows()
